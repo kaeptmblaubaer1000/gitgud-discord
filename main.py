443310cd0529414534deb1e3e9da350f20e92eb5
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-02-24 04:40:19 +0100

    Add output when bot starts

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="git ", description="Git Gud - a utility for when you are told to 'git gud'")
 bot.load_extension("gitgud_discord")
+
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    await bot.change_presence(activity=discord.Game(name="Git Gud (https://github.com/kaeptmblaubaer1000/gitgud2)"))
+    print('Successfully set rich presence')
+
 with open("bot_token.txt", "r") as fp:
     token = fp.read().strip()
 bot.run(token)
