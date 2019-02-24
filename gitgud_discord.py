90862bd81aaec127a7ebecc1a13eee587592a32d
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-02-24 12:31:20 +0100

    Add git gud command

--- gitgud_discord.py
+++ gitgud_discord.py
@@ -1,8 +1,21 @@
 from discord.ext import commands
+from gitgud.gitgud import git
+import io
+import shlex
 
-class GitGudCog(commands.Cog):
+class GitGudCog(commands.Cog, name="Git Gud"):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name="gud")
+    async def gud(self, ctx, *, args=""):
+        fp = io.StringIO()
+        args = shlex.split(args)
+        git.gud(args, output=fp.write)
+        if "--super" in args:
+            await ctx.send(f"```\n{fp.getvalue()}\n```")
+        else:
+            await ctx.send(fp.getvalue())
 
 def setup(bot):
     bot.add_cog(GitGudCog(bot))
