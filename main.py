015e047beef51f4099538545cc7b4933c80f2ec4
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-03-03 07:16:25 +0100

    Setup logging

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import discord
 from discord.ext import commands
+import logging
+import sys
+logger = logging.getLogger("gitgud_discord").getChild(__name__)
+handler = logging.StreamHandler(sys.stdout)
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger(discord.__name__).setLevel(logging.INFO)
 
-bot = commands.Bot(command_prefix="git ", description="Git Gud - a utility for when you are told to 'git gud'")
+bot = commands.Bot(command_prefix="git ",
+                   description="Git Gud - a utility for when you are told to 'git gud'")
 bot.load_extension("gitgud_discord")
 bot.load_extension("jishaku")
 
+
 @bot.event
 async def on_ready():
-    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    print(
+        f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
     await bot.change_presence(activity=discord.Game(name="Git Gud (https://github.com/kaeptmblaubaer1000/gitgud2)"))
     print('Successfully set rich presence')
+    logger.info(bot.guilds)
 
 with open("bot_token.txt", "r") as fp:
     token = fp.read().strip()
