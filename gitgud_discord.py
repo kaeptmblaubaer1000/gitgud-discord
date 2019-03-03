d655e53ee412a9bedd7a45c4f7bc8fe186f6754e
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-03-03 07:18:03 +0100

    Catch SystemExits

--- gitgud_discord.py
+++ gitgud_discord.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 from gitgud.gitgud import git
 import io
 import shlex
+import sys
+
 
 class GitGudCog(commands.Cog, name="Git Gud"):
     def __init__(self, bot):
@@ -12,10 +14,23 @@ class GitGudCog(commands.Cog, name="Git Gud"):
 
         @commands.command(name=name)
         async def command(self, ctx, *, args=""):
+            err = 0
             fp = io.StringIO()
+            errfp = io.StringIO()
             args = shlex.split(args)
-            gudcommand(args, output=fp.write)
-            if "--super" in args:
+            dstderr = sys.stderr
+            sys.stderr = errfp
+            dargs = sys.argv
+            sys.argv = [f"{ctx.prefix}{ctx.command.qualified_name}"] + args
+            try:
+                gudcommand(args, output=fp.write)
+            except SystemExit as se:
+                err = se.args[0]
+            sys.argv = dargs
+            sys.stderr = dstderr
+            if err:
+                await ctx.send(f"```\nError {err}:\n{errfp.getvalue()}```")
+            elif "--super" in args:
                 await ctx.send(f"```\n{fp.getvalue()}\n```")
             else:
                 await ctx.send(fp.getvalue())
@@ -27,6 +42,7 @@ class GitGudCog(commands.Cog, name="Git Gud"):
     spooked = _make_command("spooked")
     job = _make_command("job")
     money = _make_command("money")
+
 
 def setup(bot):
     bot.add_cog(GitGudCog(bot))
