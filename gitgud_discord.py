a3e1c468d2d8907e9f89fcc4f8803e8719681ae4
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-03-08 17:11:23 +0100

    Add a few improvements and add a new 'ping' command

--- gitgud_discord.py
+++ gitgud_discord.py
@@ -5,6 +5,8 @@ import shlex
 import sys
 
 
+cleaner = commands.clean_content(fix_channel_mentions=True)
+
 class GitGudCog(commands.Cog, name="Git Gud"):
     def __init__(self, bot):
         self.bot = bot
@@ -14,12 +16,14 @@ class GitGudCog(commands.Cog, name="Git Gud"):
 
         @commands.command(name=name)
         async def command(self, ctx, *, args=""):
+            args = await cleaner.convert(ctx, args)
             err = 0
             fp = io.StringIO()
-            errfp = io.StringIO()
             args = shlex.split(args)
             dstderr = sys.stderr
-            sys.stderr = errfp
+            dstdout = sys.stdout
+            sys.stderr = fp
+            sys.stdout = fp
             dargs = sys.argv
             sys.argv = [f"{ctx.prefix}{ctx.command.qualified_name}"] + args
             try:
@@ -28,9 +32,11 @@ class GitGudCog(commands.Cog, name="Git Gud"):
                 err = se.args[0]
             sys.argv = dargs
             sys.stderr = dstderr
+            sys.stdout = dstdout
+            s = fp.getvalue()
             if err:
-                await ctx.send(f"```\nError {err}:\n{errfp.getvalue()}```")
-            elif "--super" in args:
+                await ctx.send(f"```\nError {err}:\n{s}```")
+            elif "\n" in s:
                 await ctx.send(f"```\n{fp.getvalue()}\n```")
             else:
                 await ctx.send(fp.getvalue())
@@ -42,6 +48,10 @@ class GitGudCog(commands.Cog, name="Git Gud"):
     spooked = _make_command("spooked")
     job = _make_command("job")
     money = _make_command("money")
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        await ctx.send("You got a ping!")
 
 
 def setup(bot):
