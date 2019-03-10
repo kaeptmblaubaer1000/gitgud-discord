10c3a86f8f370de118ebec7290c07f11c4d73bb5
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-03-10 18:15:47 +0100

    Let GH URL point to bot repository

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot.load_extension("jishaku")
 async def on_ready():
     print(
         f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    await bot.change_presence(activity=discord.Game(name="Git Gud (https://github.com/kaeptmblaubaer1000/gitgud2)"))
+    await bot.change_presence(activity=discord.Game(name="Git Gud (https://github.com/kaeptmblaubaer1000/gitgud-discord)"))
     print('Successfully set rich presence')
     logger.info(bot.guilds)
 
